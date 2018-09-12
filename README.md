717c620d8f1d20da75b7b180c3dc4aa843721772
ДимК <dimaszone@hotmail.com>
2018-09-12 20:42:51 +0300

    adding project

--- README.md
+++ README.md
@@ -1,2 +1,183 @@
 # SP.CoreSearch
-Sharepoint postquery js engine
+SharePoint postquery javascript engine
+
+- searching
+- sorting
+- filtering
+- rifiner results
+- paging results
+
+## Dependencies
+
+* es6 Promise (or use [polyfill](https://github.com/stefanpenner/es6-promise))
+
+## Demo
+
+![image](https://github.com/d-kochanzhi/SP.CoreSearch/raw/master/src/sp.core.search.gif)
+
+##### this working demo (fetching data, sorting, filtering, paging and refining) contains only 50 rows of unminimize js code
+
+## Demo source (with VueJs)
+
+### Include js to sharepoint page header
+
+```html  
+  <script src='<%= SPUtility.MakeBrowserCacheSafeLayoutsUrl("vue.js", false) %>'></script>
+  <script src='<%= SPUtility.MakeBrowserCacheSafeLayoutsUrl("es6-promise.auto.min.js", false) %>'></script>
+  <script src='<%= SPUtility.MakeBrowserCacheSafeLayoutsUrl("SP.CoreSearch.js", false) %>'></script>
+```
+### add to page content
+
+```html  
+
+ <div id="app">
+    <div class="row">
+    <div class="col-md-1"></div>
+        <div class="col-md-8">
+            <div class="sourcesContainer">         
+                <input type="text" v-model="filter.title" placeholder="Search by Title..."  v-on:keyup.enter="doSearch(1)" class="txtSearch" />                      
+            </div>   
+            <div class="search-message-no-results"  v-if="!hasResults"><h3>Ups...no results</h3></div>
+            <div class="panel panel-default" v-else>          
+                <div class="panel-body">    
+                <table class="table table-striped">
+                    <thead>
+                    <tr>                  
+                        <th>ID</th>
+                        <th>Title</th> 
+                        <th>SeoKeywords</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in result.PrimaryResults" v-bind:key="item.ListItemID">  
+                            <td>{{item.ListItemID}}</td>
+                            <td>{{item.Title}}</td> 
+                            <td>{{item.SeoKeywords}}</td> 
+                        </tr>
+                    </tbody>
+                </table>                
+                </div>
+            </div>
+            <ul class="containerPager">
+                <li v-for="page in result.TotalPages" v-bind:class="{pageButton: true, current: page===result.CurrentPage }"><a href="#" v-on:click.prevent="doSearch(page)">{{page}}</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <div class="sourcesContainer" v-for="refiner in result.RefinementResults">       
+                <div>{{refiner.Name}}</div>
+                    <ul>
+                        <li v-for="refinerItem in refiner.Items"><a href="#" v-on:click.prevent="setRefinerFilter(refiner.Name, refinerItem)">{{refinerItem.RefinementName}} ({{refinerItem.RefinementCount}})</a> <span v-show="isActiveRefiner(refiner.Name, refinerItem)">&nbsp;&nbsp;X</span></li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+ </div>
+<script type="text/javascript">
+        var app = new Vue({
+            el: '#app',           
+            data: {
+                filter: {},
+                settings: {},
+                result: {},
+                __refiners: {}
+            },
+            mounted: function () {
+                var self = this;
+                /* uncomment this if scripts not loaded automatically on page
+                SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () { });
+                SP.SOD.executeFunc('sp.runtime.js', null, function () { });
+                SP.SOD.executeFunc('init.js', null, function () { });
+                */
+                ExecuteOrDelayUntilScriptLoaded(function () {
+                    self.onLoad();
+                }, "SP.CoreSearch");
+
+            },
+            computed: {
+                hasResults: function () {
+                    return this.result.PrimaryResults && this.result.PrimaryResults.length > 0;
+                },
+            },
+            methods: {
+                onLoad: function () {
+                    this.settings = new SP.CoreSearch.Settings();
+                    this.settings.SelectProperties = ["ListItemID", "Title", "SeoKeywords"];
+                    this.settings.QueryTemplate = "{searchterms} ContentTypeId:0x010042A8B2213293034DADE192C571DC80A9*";
+                    this.settings.Refiners = "SeoKeywords";
+
+                    this.doSearch(1);
+                },
+                doSearch: function (page) {
+                    var self = this;
+                    this.settings.Querytext = this.getQuery();
+                    SP.CoreSearch.SearchEngine.Instance.Request(this.settings, page, 4)
+                        .then(function (data) {
+                            self.result = data;
+                        }, function (err) {
+                            console.log(err);
+                        });
+                },
+                getQuery: function () {                   
+                    return this.filter.title ? ('Title:*' + this.filter.title + '*') : '';
+                },                
+                setRefinerFilter: function (refiner, refinement) {
+                    this.__refiners = this.settings.ApplyRefiner(refiner,refinement.RefinementValue, refinement.RefinementToken);                   
+                    this.doSearch(1);
+                },
+                isActiveRefiner: function (refiner, refinement) {
+                    return this.__refiners && this.__refiners[refiner] && this.__refiners[refiner].indexOf(refinement.RefinementValue) > -1;
+                }
+            }
+        });
+    </script>
+
+```
+
+All you need is splist
+![image](https://github.com/d-kochanzhi/SP.CoreSearch/raw/master/src/standart_list.png)
+And enable rifine property on field
+![image](https://github.com/d-kochanzhi/SP.CoreSearch/raw/master/src/managed_properties.png)
+
+## SP.CoreSearch.Settings Properties
+
+Option | Type | Description | Default
+------------|-----------|-------------|------------
+Culture|String|''|'1049'
+EnableQueryRules|Bool|''|false
+TrimDuplicates|Bool|''|false
+SelectProperties|Array|''|["Title"]
+Querytext|String|''|''
+QueryTemplate|String|''|'{searchterms}'
+StartRow|Integer|''|0
+RowLimit|Integer|''|10
+SortList|Array|''|[]
+RefinementFilters|Array|''|[]
+Refiners|String|''|''
+SourceId|String|''|''
+RankingModelId|String|''|''
+Instance|Object|new SP.CoreSearch.Settings()|```instanse of class```
+### SP.CoreSearch.Settings Methods
+
+Function | Params | Description 
+------------|-----------|-------------
+ApplyRefiner|field, value, token|''
+
+
+
+## SP.CoreSearch.SearchEngine Properties
+
+Option | Type | Description | Default
+-------|------|-------------|--------
+Instance|Object|new SP.CoreSearch.SearchEngine()|```instanse of class```
+
+### SP.CoreSearch.SearchEngine Methods
+
+Function | Params | Return Object 
+------------|-----------|-------------
+Request|settings, page, itemsperpage|```{TotalPages: 0,TotalRows: 0,CurrentPage: 0,PrimaryResults: [],RefinementResults: []}```
+
+
+## License
+
+This project is licensed under the MIT License
+
---
+++ src/SP.Core.js
@@ -0,0 +1,311 @@
+/*
+ Dmitry Kochanzhi
+ https://github.com/d-kochanzhi
+ SP.CoreSearch.SearchEngine.Instance.Request(new SP.CoreSearch.Settings());
+ */
+(function () {
+    'use strict';
+    ExecuteOrDelayUntilScriptLoaded(function () {
+        if (SP.CoreSearch) return;
+        Type.registerNamespace('SP.CoreSearch');
+
+        SP.CoreSearch.Settings = function () {
+            this.Culture = "1049";
+            this.EnableQueryRules = false;
+            this.TrimDuplicates = false;
+            this.SelectProperties = ["Title"];
+            this.Querytext = '';
+            this.QueryTemplate = '{searchterms}';
+            this.StartRow = 0;
+            this.RowLimit = 10;
+            this.SortList = [];
+            this.RefinementFilters = [];
+            this.Refiners = "";
+            this.SourceId = "";
+            this.RankingModelId = "";
+
+        }
+        SP.CoreSearch.Settings.prototype.PrepareQuery = function () {
+            var query = {
+                "__metadata": {
+                    "type": "Microsoft.Office.Server.Search.REST.SearchRequest"
+                },
+                "Culture": this.Culture,
+                "UILanguage": this.Culture,
+                "EnableQueryRules": this.EnableQueryRules,
+                "TrimDuplicates": this.TrimDuplicates,
+                "SelectProperties": {
+                    "results": this.SelectProperties
+                },
+                "Querytext": this.Querytext,
+                "QueryTemplate": this.QueryTemplate,
+                "StartRow": this.StartRow,
+                "RowLimit": this.RowLimit,
+                "EnableSorting": this.SortList.length > 0 ? true : false,
+                "SortList": {
+                    "results": this.SortList
+                },
+                "RefinementFilters": {
+                    "results": this.RefinementFilters
+                },
+                "Refiners": this.Refiners,
+                "SourceId": this.SourceId,
+                "RankingModelId": this.RankingModelId
+            };
+
+            if (!this.SourceId)
+                delete query.SourceId;
+
+            if (!this.RankingModelId)
+                delete query.RankingModelId;
+
+            return query;
+        }
+        SP.CoreSearch.Settings.prototype.ApplyRefiner = function (field, value, token) {
+            var self = this;
+            this.__refiners = this.__refiners || {};
+            if (!this.__refiners[field]) {
+                this.__refiners[field] = [];
+            }
+
+            if (value)
+                if (this.__refiners[field].indexOf(value) >= 0) {
+                    this.__refiners[field].splice(this.__refiners[field].indexOf(value), 1);
+                } else {
+                    this.__refiners[field].push(value);
+                }
+
+            if (token)
+                if (this.__refiners[field].indexOf(token) >= 0) {
+                    this.__refiners[field].splice(this.__refiners[field].indexOf(token), 1);
+                } else {
+                    this.__refiners[field].push(token);
+                }
+
+            if (!this.__refiners[field].length) {
+                delete this.__refiners[field];
+            }
+
+            self.RefinementFilters.splice(0, self.RefinementFilters.length);
+            Object.getOwnPropertyNames(this.__refiners).forEach(function (item) {
+                var refStr = "";
+                self.__refiners[item].forEach(function (i) {
+                    if (i.indexOf("??") > -1)
+                        refStr += ",{0}".format(i);
+                    else
+                        refStr += ",equals(\"{0}\")".format(i);
+                });
+                self.RefinementFilters.push("{0}:or(".format(item) + refStr.substring(1) + ")");
+            });      
+
+            return this.__refiners;
+        },
+        SP.CoreSearch.Settings.registerClass('SP.CoreSearch.Settings');
+        SP.CoreSearch.Settings.Instance = new SP.CoreSearch.Settings();
+
+        SP.CoreSearch.SearchEngine = function () {
+            this.__digest = null;
+            this.__settings = new SP.CoreSearch.Settings();
+        };
+        SP.CoreSearch.SearchEngine.prototype.Request = function (settings, page, itemsperpage) {
+            var self = this;
+
+            if (settings !== null && settings instanceof SP.CoreSearch.Settings)
+                this.__settings = settings;
+            else
+                this.__settings = new SP.CoreSearch.Settings();
+
+            // override settings if got params
+            if (itemsperpage) {
+                this.__settings.RowLimit = itemsperpage;
+            }
+            if (page) {
+                this.__settings.StartRow = (page - 1) * this.__settings.RowLimit;
+            }
+            // end override
+
+            return new Promise(function (resolve, reject) {
+                try {
+                    self.__requestDigest().then(
+                        function (digest) {
+                            var url = "/_api/search/postquery?_=" + (new Date()).getTime();
+                            var data = JSON.stringify({ 'request': self.__settings.PrepareQuery() });
+                            var headers = {
+                                "accept": "application/json;odata=verbose",
+                                "Content-Type": "application/json;odata=verbose",
+                                "X-RequestDigest": digest.FormDigestValue
+                            };
+
+                            self.__httpPost(url, data, headers)
+                                .then(function (response) {
+                                    resolve(self.__prepareSearchResults(response));
+                                }, function (err) {
+                                    if (window.console && console.log) {
+                                        console.log(err);
+                                    }
+                                    reject();
+                                });
+                        });
+
+                }
+                catch (e) {
+                    reject(e);
+                }
+            });
+        };
+        SP.CoreSearch.SearchEngine.registerClass('SP.CoreSearch.SearchEngine');
+        SP.CoreSearch.SearchEngine.Instance = new SP.CoreSearch.SearchEngine();
+
+        /// #region private
+        SP.CoreSearch.SearchEngine.prototype.__httpPost = function (url, data, headers) {
+            var self = this;
+
+            return new Promise(function (resolve, reject) {
+
+                var xhr = typeof XMLHttpRequest != 'undefined'
+                    ? new XMLHttpRequest()
+                    : new ActiveXObject('Microsoft.XMLHTTP');
+
+                xhr.open("POST", url, true);
+
+                if (headers)
+                    Object.getOwnPropertyNames(headers).forEach(function (h) {
+                        xhr.setRequestHeader(h, headers[h]);
+                    });
+                xhr.responseType = 'json';
+
+                xhr.onload = function () {
+                    if (xhr.status == 200) {
+                        resolve(xhr.response);
+                    } else {
+                        var error = new Error(xhr.statusText);
+                        error.code = xhr.status;
+                        reject(error);
+                    }
+                };
+
+                xhr.onerror = function () {
+                    reject(new Error("Network Error"));
+                };
+
+                xhr.send(data);
+            });
+        }
+        SP.CoreSearch.SearchEngine.prototype.__requestDigest = function () {
+            var self = this;
+
+            return new Promise(function (resolve, reject) {
+                if (self.__digest && self.__digest.Date > new Date())
+                    resolve(self.__digest);
+                else {
+                    var url = "/_api/contextinfo?_=" + (new Date()).getTime();
+                    var headers = {
+                        "accept": "application/json;odata=verbose"
+                    };
+
+                    self.__httpPost(url, null, headers)
+                        .then(function (data) {
+                            self.__digest = data.d.GetContextWebInformation;
+                            self.__digest.Date = new Date(new Date().getTime() + self.__digest.FormDigestTimeoutSeconds * 1000);
+                            resolve(self.__digest);
+                        }, function (err) {
+                            if (window.console && console.log) {
+                                console.log(err);
+                            }
+                            reject();
+                        });
+                }
+            });
+        }
+        SP.CoreSearch.SearchEngine.prototype.__prepareSearchResults = function (results) {
+            var responseObj = {
+                TotalPages: 0,
+                TotalRows: 0,
+                CurrentPage: 0,
+                PrimaryResults: [],
+                RefinementResults: []
+            };
+
+            if (results && typeof results === "object") {
+                if (results.d.postquery.PrimaryQueryResult) {
+                    //PrimaryResults
+                    var resultInfo = results.d.postquery.PrimaryQueryResult.RelevantResults;
+                    if (resultInfo) {
+                        responseObj.TotalRows = resultInfo.TotalRows;
+                        responseObj.PrimaryResults = this.__convertPrimaryResultsToObjectArray(resultInfo.Table.Rows.results);
+                        responseObj.TotalPages = Math.ceil(responseObj.TotalRows / this.__settings.RowLimit);
+                        responseObj.CurrentPage = Math.ceil(this.__settings.StartRow / this.__settings.RowLimit) + 1;
+                    }
+                    //RefinementResults
+                    var refinerInfo = results.d.postquery.PrimaryQueryResult.RefinementResults;
+                    if (refinerInfo) {
+                        responseObj.RefinementResults = this.__convertRefinersResultsToObjectArray(refinerInfo.Refiners.results);
+                    }
+                }
+            }
+
+            return responseObj;
+        }
+        SP.CoreSearch.SearchEngine.prototype.__convertPrimaryResultsToObjectArray = function (results) {
+            if (!results || results.length === 0) return [];
+
+            var retval = [];
+            for (var i = 0; i < results.length; i++) {
+                retval.push(this.__convertPrimaryResultsItemToObject(results[i]));
+            }
+
+            return retval;
+        }
+        SP.CoreSearch.SearchEngine.prototype.__convertPrimaryResultsItemToObject = function (result) {
+            var item = {};
+            var dataCells = result.Cells.results;
+            for (var j = 0; j < dataCells.length; j++) {
+                var cell = dataCells[j];
+                if (cell.Value && (typeof cell.Value !== "string" || cell.Value.trim())) {
+                    var val = cell.Value;
+                    switch (cell.ValueType) {
+                        case "Edm.Double":
+                            val = parseFloat(val);
+                            break;
+                        case "Edm.Int64":
+                            val = parseInt(val);
+                            break;
+                        case "Edm.Boolean":
+                            val = val === "true";
+                    }
+
+                    item[cell.Key] = val;
+                }
+            }
+
+            return item;
+        }
+        SP.CoreSearch.SearchEngine.prototype.__convertRefinersResultsToObjectArray = function (results) {
+            if (!results || results.length === 0) return [];
+
+            var retval = [];
+            for (var i = 0; i < results.length; i++) {
+                retval.push(this.__convertRefinersResultsItemToObject(results[i]));
+            }
+
+            return retval;
+        }
+        SP.CoreSearch.SearchEngine.prototype.__convertRefinersResultsItemToObject = function (result) {
+            var item = {};
+            item.Name = result.Name;
+            item.Items = result.Entries.results;
+            return item;
+        }
+        /// end region
+
+        if (typeof (Sys) !== "undefined" && Sys && Sys.Application) {
+            Sys.Application.notifyScriptLoaded();
+        }
+
+        NotifyScriptLoadedAndExecuteWaitingJobs("SP.CoreSearch");
+
+
+    }, "sp.init.js");
+
+
+})();
